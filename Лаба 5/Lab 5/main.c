95da86bd507e67e36283e38c95d5440089e56e2f
MyStaRiK <117556862+MyStaRiK@users.noreply.github.com>
2023-05-13 17:35:36 +0800

    Add files via upload

---
+++ Лаба 5/Lab 5/main.c
@@ -0,0 +1,237 @@
+#include <windows.h>
+#include <gl/gl.h>
+#include <math.h>
+
+float vertex [] = {1,1,0,  1,-1,0,  -1,-1,0,  -1,1,0};
+float Xalfha = 10;
+float Z = 0;
+POINTFLOAT position = {0,0};
+float piramid [] = {0,0,2, 1,1,0, 1,-1,0, -1,-1,0, -1,1,0, 1,1,0};
+
+void Show(){
+    glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3,GL_FLOAT, 0, &vertex);
+        for (int i =0 ; i< 5; i++)
+            for (int j=0; j<5; j++)
+        {
+            glPushMatrix();
+                if ((i+j)%2==0) glColor3f(0.5,0.5,0.5);
+                else glColor3f(1,1,1);
+                glTranslatef(i*2, j*2, 0);
+                glDrawArrays(GL_TRIANGLE_FAN,0,4);
+            glPopMatrix();
+        }
+
+        glVertexPointer(3,GL_FLOAT, 0, &piramid);
+        glColor3f(1,0.5,0.5);
+        glDrawArrays(GL_TRIANGLE_FAN,0,6);
+
+        glPushMatrix();
+            glTranslatef(3,3,0);
+            glScalef(1,1,0.5);
+            glColor3f(1,0.5,0.5);
+            glDrawArrays(GL_TRIANGLE_FAN,0,6);
+        glPopMatrix();
+
+        glPushMatrix();
+            glTranslatef(-3,3,0);
+            glRotatef(45,0,0,1);
+            glColor3f(1,0.5,0.5);
+            glDrawArrays(GL_TRIANGLE_FAN,0,6);
+        glPopMatrix();
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+void Move(){
+    if (GetKeyState(VK_UP) < 0) Xalfha = ++Xalfha > 180 ? 180 : Xalfha;
+    if (GetKeyState(VK_DOWN) < 0) Xalfha = --Xalfha < 0 ? 0 : Xalfha;
+    if (GetKeyState(VK_LEFT) < 0) Z--;
+    if (GetKeyState(VK_RIGHT) < 0) Z++;
+
+    float ugol = -Z / 180 * M_PI;
+    float speed =0;
+    if (GetKeyState('W') < 0) speed = 0.1;
+    if (GetKeyState('S') < 0) speed = -0.1;
+    if (GetKeyState('A') < 0) {speed = 0.1; ugol -= M_PI*0.5;}
+    if (GetKeyState('D') < 0) {speed = 0.1; ugol += M_PI*0.5;}
+    if (speed!=0)
+    {
+        position.x += sin(ugol) * speed;
+        position.y += cos(ugol) * speed;
+    }
+
+    glRotatef(-Xalfha,1,0,0);
+    glRotatef(Z,0,0,1);
+    glTranslatef(-position.x,-position.y,-3);
+}
+
+LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+void EnableOpenGL(HWND hwnd, HDC*, HGLRC*);
+void DisableOpenGL(HWND, HDC, HGLRC);
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
+{
+    WNDCLASSEX wcex;
+    HWND hwnd;
+    HDC hDC;
+    HGLRC hRC;
+    MSG msg;
+    BOOL bQuit = FALSE;
+    float theta = 0.0f;
+
+    /* register window class */
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_OWNDC;
+    wcex.lpfnWndProc = WindowProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = "GLSample";
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);;
+
+
+    if (!RegisterClassEx(&wcex))
+        return 0;
+
+    /* create main window */
+    hwnd = CreateWindowEx(0,
+                          "GLSample",
+                          "OpenGL Sample",
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          1024,
+                          1024,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hwnd, nCmdShow);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL(hwnd, &hDC, &hRC);
+
+    glFrustum(-1,1, -1,1, 2,80);
+    glEnable(GL_DEPTH_TEST);
+
+    /* program main loop */
+    while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glPushMatrix();
+                Move();
+                Show();
+            glPopMatrix();
+
+            SwapBuffers(hDC);
+
+            Sleep (1);
+        }
+    }
+
+    /* shutdown OpenGL */
+    DisableOpenGL(hwnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow(hwnd);
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+        break;
+
+        case WM_DESTROY:
+            return 0;
+
+        case WM_KEYDOWN:
+        {
+            switch (wParam)
+            {
+                case VK_ESCAPE:
+                    PostQuitMessage(0);
+                break;
+            }
+        }
+        break;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+void EnableOpenGL(HWND hwnd, HDC* hDC, HGLRC* hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC(hwnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory(&pfd, sizeof(pfd));
+
+    pfd.nSize = sizeof(pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW |
+                  PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+
+    iFormat = ChoosePixelFormat(*hDC, &pfd);
+
+    SetPixelFormat(*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext(*hDC);
+
+    wglMakeCurrent(*hDC, *hRC);
+}
+
+void DisableOpenGL (HWND hwnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(hRC);
+    ReleaseDC(hwnd, hDC);
+}
+
